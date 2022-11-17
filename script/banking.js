d23e82b72d4338ef3851cd0ca9bbcd9a6fcf64a7
fawziaruma <fawziaruma0652@gmail.com>
2022-11-17 09:45:47 +0600

    last commit

--- script/banking.js
+++ script/banking.js
@@ -9,4 +9,35 @@ document.getElementById('deposite-button').addEventListener('click', function(){
     const previousDepositeAmount = parseFloat(previousDepositeText)
     const newDepositeTotal = previousDepositeAmount + newDepositeAmount
     depositeTotal.innerText=newDepositeTotal
+ 
+    // update account balance
+    const balanceTotal = document.getElementById('balance-total')
+    const balanceTotalText = balanceTotal.innerText
+    const previousBalanceAmount = parseFloat(balanceTotalText)
+    const newBalanceTotal = previousBalanceAmount + newDepositeTotal
+    balanceTotal.innerText = newBalanceTotal
+
+    //clear the deposite input field 
+    depositeInput.value =""
 })
+    //   handle withdrow even handelar
+document.getElementById('withdrow-button').addEventListener('click', function(){
+    const withdrowInput = document.getElementById('withdrow-input')
+    const newWithdrowAmountText = withdrowInput.value
+    const newWithdrowAmount = parseFloat(newWithdrowAmountText)
+
+    const withdrowTotal = document.getElementById('withdrow-total')
+    const previousWithdrowText = withdrowTotal.innerText
+    const previousWithdrowAmount = parseFloat(previousWithdrowText)
+    const newWithdrowTotal = newWithdrowAmount + previousWithdrowAmount
+    withdrowTotal.innerText = newWithdrowTotal 
+
+    // update balance
+    const balanceTotal = document.getElementById('balance-total')
+    const previousBalanceText = balanceTotal.innerText
+    const previousBalanceTotal = parseFloat(previousBalanceText)
+    const newBalanceTotal = previousBalanceTotal - newWithdrowTotal;
+    balanceTotal.innerText = newBalanceTotal
+    withdrowInput.value = ""
+})
+
